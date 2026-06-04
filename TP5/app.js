373b637a24b8f4c339347b5a6b6a6021c7a8e818
alumno <alumno@Instituto.local>
2026-06-04 08:04:56 -0300

    edite el app.js

---
+++ TP5/app.js
@@ -0,0 +1,37 @@
+//ej1 suma
+let numero1 = 25;
+let numero2 = 23;
+let resultado
+resultado = numero1 + numero2
+alert(resultado);
+//ej1 res
+numero1 = 22;
+ numero2 = 20;
+ resultado
+
+resultado = numero1 - numero2
+
+console.log(resultado);
+alert(resultado);
+//ej3 multi
+numero1 = 20;
+ numero2 = 3;
+ resultado
+
+resultado = numero1 * numero2
+
+console.log(resultado);
+alert(resultado);
+//ej4 div
+numero1 = 20;
+ numero2 = 2;
+ resultado
+
+resultado = numero1 / numero2
+
+console.log(resultado);
+alert(resultado);
+
+
+
+
