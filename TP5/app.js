4b630b95cd6ea89d6ae72a1fe8227d8a6bfc056e
alumno <alumno@Instituto.local>
2026-06-11 08:25:52 -0300

    Update app.js

--- TP5/app.js
+++ TP5/app.js
@@ -4,6 +4,7 @@ let numero2 = 23;
 let resultado
 resultado = numero1 + numero2
 alert(resultado);
+console.log("El resultado de suma es:" + resultado)
 //ej1 res
 numero1 = 22;
  numero2 = 20;
@@ -11,7 +12,7 @@ numero1 = 22;
 
 resultado = numero1 - numero2
 
-console.log(resultado);
+console.log('el resultado de la resta es:' + resultado);
 alert(resultado);
 //ej3 multi
 numero1 = 20;
@@ -20,7 +21,7 @@ numero1 = 20;
 
 resultado = numero1 * numero2
 
-console.log(resultado);
+console.log('el resultado de la multiplicacion es:' + resultado);
 
 //ej4 div
 numero1 = 20;
@@ -29,7 +30,7 @@ numero1 = 20;
 
 resultado = numero1 / numero2
 
-console.log(resultado);
+console.log('el resultado de la division es:' + resultado);
 
 
 let precio1 = 140
@@ -37,5 +38,12 @@ let precio2 = 450
 let precio3 = 100
 
 resultado = precio1 + precio2 + precio3
-
 console.log(resultado)
+
+let nombreAluno = 'simo';
+let apellidoAlumno = 'sanchez';
+let edadAlumno = 17;
+
+console.log(nombreAluno + ' '+ apellidoAlumno +' '+ 'tiene' +' '+ edadAlumno);
+
+
