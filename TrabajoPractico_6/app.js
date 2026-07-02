718c19f91d8a77f6b9dceaecd4c07eaf331e3193
alumno <alumno@Instituto.local>
2026-07-02 09:21:02 -0300

    2/7

--- TrabajoPractico_6/app.js
+++ TrabajoPractico_6/app.js
@@ -3,7 +3,8 @@ let dos = document.querySelector('#b2')
 let tres = document.querySelector('#b3')
 let cuatro = document.querySelector('#b4')
 let p = document.querySelector('p')
-
+let ct = document.querySelector('#ct')
+let r = document.querySelector('#r')
 
 uno.onclick = function(){
     //cambiar el color de fondo
@@ -20,5 +21,14 @@ tres.onclick = function(){
 cuatro.onclick = function(){
     p.style.fontFamily = 'IMPACT'
 }
-
-
+ct.onclick = function(){
+    //cambia el tamaño del texto
+    p.style.fontSize = '50px'
+}
+r.onclick = function(){
+p.style.color = 'rgb(246, 253, 255)'
+p.style.fontFamily = 'Arial'
+p.style.backgroundColor = 'rgb(171, 202, 230)'
+p.style.fontSize = '25px'
+p.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet vitae, fugiat at perspiciatis incidunt suscipit eum similique sapiente id repellendus? Molestiae odio qui perferendis quos. Eveniet impedit voluptates officia voluptatum!'
+}
